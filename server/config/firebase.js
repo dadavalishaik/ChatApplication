5aedbf7be50daa55d5720ec1a7d14fe7f31a1b4e
dadavalishaik <shaikdadavali092@gmail.com>
2025-09-02 21:03:43 +0530

    feat(firebase authentication)

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -1,8 +1,9 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = require("../config/serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://chatapp-49465.firebaseio.com",
 });
 
 module.exports = admin;
